ff271462dfcda5befe61492f8dc312bcd1424236
Justin Leitgeb <justin@stackbuilders.com>
2015-03-24 17:07:40 -0500

    Docs

--- src/Text/ParseErrorEq.hs
+++ src/Text/ParseErrorEq.hs
@@ -1,16 +1,29 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 
+{-|
+Module      : Text.ParseErrorEq
+Description : Adds an Eq instance for Parsec's ParseError type if needed
+Copyright   : (c) Stack Builders Inc., 2015
+                  Someone Else, 2014
+License     : GPL-3
+Maintainer  : justin@stackbuilders.com
+Stability   : experimental
+Portability : POSIX
+
+This module adds an Eq instance for Parsec's ParseError type if it's
+needed. Import by using (since this module only provides instances):
+
+> import Text.ParseErrorEq ()
+
+For reference, Parsec 3.1.9 adds the Eq instance, in the commit here:
+
+https://github.com/aslatter/parsec/commit/3fb40aaa683fd90f3f0dd9f3c32c6ba707fb24b1
+-}
 module Text.ParseErrorEq where
 
 #if MIN_VERSION_parsec(3,1,9)
 
-{-|
-Parsec's ParseError already contains an Eq definition in this version,
-so there is no need to add it.  The Eq instance was added to Parsec in
-version 3.1.9, in this commit:
-
-https://github.com/aslatter/parsec/commit/3fb40aaa683fd90f3f0dd9f3c32c6ba707fb24b1
--}
+-- ParseError Eq instance already defined in Parsec, not necessary to add.
 
 #else
 
@@ -18,8 +31,8 @@ import Text.Parsec.Error (ParseError)
 import Text.ParserCombinators.Parsec.Error
   (errorPos, errorMessages, messageString)
 
--- This implementation of Eq for ParseError is the same as the one
--- added in Parsec in the above commit.
+--| This implementation of Eq for ParseError is the same as the one
+--  added in Parsec in the above commit.
 instance Eq ParseError where
   l == r
        = errorPos l == errorPos r && messageStrs l == messageStrs r
